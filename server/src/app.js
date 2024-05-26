f1a06498790641926c136732cce2e6e8cb213ccf
Dauren <213053@astanait.edu.kz>
2024-05-26 09:23:01 +0500

    before deploy

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import api from './routes/api.js'
 
 const app = express()
-app.use(cors({origin: '*'}))
+app.use(cors({origin: 'https://n-factorial-ai-cup-2024-front-6010uuq6n.vercel.app/'}))
 app.use(express.json())
 
 app.use('/api/v1/', api)
