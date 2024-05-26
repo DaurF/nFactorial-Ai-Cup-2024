a514c6914e8a3fcc3a4673f2f2192033caed6b80
Dauren <213053@astanait.edu.kz>
2024-05-26 09:32:02 +0500

    cors error fix

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,10 @@ import api from './routes/api.js'
 
 const app = express()
 
-app.all('/', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
