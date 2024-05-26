0a8b23a41e0d4cf764e360823ce5fa0036936002
Dauren <213053@astanait.edu.kz>
2024-05-26 09:29:52 +0500

    cors error

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,14 @@
 import express from 'express'
-import cors from 'cors'
 import api from './routes/api.js'
 
 const app = express()
-app.use(cors({
-  origin: 'https://n-factorial-ai-cup-2024-front-6010uuq6n.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}))
+
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(express.json())
 
 app.use('/api/v1/', api)
