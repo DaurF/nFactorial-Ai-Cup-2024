6e26c5490fa141fd36d49b044158687177bd1217
Dauren <213053@astanait.edu.kz>
2024-05-26 09:19:43 +0500

    changed cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import api from './routes/api.js'
 
 const app = express()
-app.use(cors())
+app.use(cors({origin: '*'}))
 app.use(express.json())
 
 app.use('/api/v1/', api)
